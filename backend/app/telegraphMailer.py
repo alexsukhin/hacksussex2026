c66ee5c9336598f0f6a2b967331c356df91fe4ee
Lewis Corry <lewiscorry12@gmail.com>
2026-03-01 11:49:00 +0000

    finalise telegram

--- backend/app/telegraphMailer.py
+++ backend/app/telegraphMailer.py
@@ -1,15 +1,13 @@
 import requests
-#https://api.telegram.org/bot8649843600:AAHgRy24e0TpCCMT6HkMGDijhPTjm4RIBS4/getUpdates]
+#https://api.telegram.org/bot8649843600:AAHgRy24e0TpCCMT6HkMGDijhPTjm4RIBS4/getUpdates]#
 
-token = "8649843600:AAHgRy24e0TpCCMT6HkMGDijhPTjm4RIBS4"
-chatID = "8717180588"
-messageBody = "Hello World"
+#token = "8649843600:AAHgRy24e0TpCCMT6HkMGDijhPTjm4RIBS4"
+#chatID = "8717180588"
+#messageBody = "Hello World"
 
 url =  f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatID}&text={messageBody}"
 r = requests.get(url)
 print(r)
-
-
 
 
 def sendUpdate():
