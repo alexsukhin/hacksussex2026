1e47d7a953f0a1b03e6b64350a9472b2eea11cf7
Alexander Sukhin <alex@sukhin.net>
2026-03-01 13:30:27 +0000

    merge

--- backend/app/telegraphMailer.py
+++ backend/app/telegraphMailer.py
@@ -1,10 +1,5 @@
 import requests
-
-url =  f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatID}&text={messageBody}"
-r = requests.get(url)
-print(r)
-
-
+x``
 def sendUpdate():
     token = "8649843600:AAHgRy24e0TpCCMT6HkMGDijhPTjm4RIBS4"
     chatID = "8717180588"
